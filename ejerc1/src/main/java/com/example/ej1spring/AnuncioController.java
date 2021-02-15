46ffda57ead60f4d85fe13d0910b1437dbd839e2
DCaceres2018 <78802315+DCaceres2018@users.noreply.github.com>
2021-02-15 14:33:05 +0100

    Añadido proyecto inicial sobre el que trabajar

---
+++ ejerc1/src/main/java/com/example/ej1spring/AnuncioController.java
@@ -0,0 +1,20 @@
+package com.example.ej1spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AnuncioController {
+
+	@PostMapping("/guardaranuncio")
+	public String guardarAnuncio(Model model, @RequestParam String nombre, @RequestParam String asunto) {
+
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("asunto", asunto);
+		
+
+		return "anuncio";
+	}
+}
