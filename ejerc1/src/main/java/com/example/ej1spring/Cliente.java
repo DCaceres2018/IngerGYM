544b5d292b250f67d52df0fc5c9dfaa93bdd0d1a
CarlosMbp <cm.barrameda.2018@alumnos.urjc.es>
2021-02-25 11:46:08 +0100

    cliente
    cliente getter y setter

--- ejerc1/src/main/java/com/example/ej1spring/Cliente.java
+++ ejerc1/src/main/java/com/example/ej1spring/Cliente.java
@@ -2,4 +2,50 @@ package com.example.ej1spring;
 
 public class Cliente {
 
+	private String usuario;
+	private String email;
+	private String edad;
+	private String nTelefono;
+	
+	Cliente(String usuario,String email,String edad, String nTelefono){
+		this.usuario= usuario;
+		this.email=email;
+		this.edad=edad;
+		this.nTelefono= nTelefono;
+	
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+	public String getnTelefono() {
+		return nTelefono;
+	}
+
+	public void setnTelefono(String nTelefono) {
+		this.nTelefono = nTelefono;
+	}
+	
+	
 }
