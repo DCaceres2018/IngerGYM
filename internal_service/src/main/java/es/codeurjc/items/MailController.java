dbac95426b9f20db7bb520286af8fb90fb67dafd
DCaceres2018 <78802315+DCaceres2018@users.noreply.github.com>
2021-04-12 16:04:31 +0200

    Servicio Interno añadido

---
+++ internal_service/src/main/java/es/codeurjc/items/MailController.java
@@ -0,0 +1,26 @@
+package es.codeurjc.items;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+	
+
+    @Autowired
+    private MailService mailService;
+
+   
+
+    @PostMapping("/sendEmail")
+	public ResponseEntity<Void> addItem(@RequestBody Message message) {
+		System.out.println("Send message: "+message.getMessageText()+" to "+message.getEmail());
+		 mailService.sendMail("ingergymdad@gmail.com",message.getEmail(),message.getAsunto(),message.getMessageText());
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+    
+}
---
+++ internal_service/src/main/java/es/codeurjc/items/Message.java
@@ -0,0 +1,40 @@
+package es.codeurjc.items;
+
+public class Message {
+
+    private String email;
+    private String messageText;
+    private String asunto;
+
+    public Message(String email, String asunto,String message) {
+        this.email = email;
+        this.messageText = message;
+        this.asunto=asunto;
+    }
+    
+    public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+
+	public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String message) {
+        this.messageText = message;
+    }
+
+}
