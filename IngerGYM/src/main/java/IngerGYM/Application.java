062fb12a8d614d94ffbc3af807ce0bf5a6814233
DCaceres2018 <d.caceres.2018@alumnos.urjc.es>
2021-05-09 22:46:38 +0200

    Cache con Hazelcast
    Actualizada la cache para funcionar con hazelcast

--- IngerGYM/src/main/java/IngerGYM/Application.java
+++ IngerGYM/src/main/java/IngerGYM/Application.java
@@ -4,30 +4,37 @@ package IngerGYM;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import org.springframework.cache.CacheManager; 
+import org.springframework.cache.annotation.EnableCaching; 
+import org.springframework.context.annotation.Bean; 
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession; 
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+
+import org.apache.commons.logging.Log;
+
 
 @EnableCaching
 @SpringBootApplication
 @EnableHazelcastHttpSession
 public class Application {
-	
-	private static final Log LOG = LogFactory.getLog(Application.class);
 
+	private static final Log LOG = LogFactory.getLog(Application.class);
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
     @Bean
     public Config config() {
 
@@ -36,21 +43,32 @@ public class Application {
         JoinConfig joinConfig = config.getNetworkConfig().getJoin();
 
         joinConfig.getMulticastConfig().setEnabled(true);
-        
-        //Necesario para la cache de Spring	
-        List<String> serversList = new ArrayList<String>();
+//Necesario para la cache de Spring	
+        /*List<String> serversList = new ArrayList<String>();
 		
 		serversList.add("ingergym1");
 		serversList.add("ingergym2");
 
 		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
-		
+		*/
 		return config;
     }
-    
-    @Bean
+//Cache de Sprinh
+   /* @Bean
     public CacheManager cacheManager() {
     	LOG.info("Activating cache...");
     	return new ConcurrentMapCacheManager("cache");
+    }
+    */
+    
+ 
+    @Bean
+    public HazelcastInstance hazelcastInstance(Config hazelCastConfig) {
+        return Hazelcast.newHazelcastInstance(hazelCastConfig);
+    }
+ 
+    @Bean
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+        return new HazelcastCacheManager(hazelcastInstance);
     }
 }
